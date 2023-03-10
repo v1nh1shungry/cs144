bb81c84f85307a3b5cb7b0790634ef0e7f84de4a
v1nh1shungry <v1nh1shungry@outlook.com>
2023-03-10 15:01:45 +0800

    lab0

---
+++ libsponge/byte_stream.cc
@@ -0,0 +1,53 @@
+#include "byte_stream.hh"
+
+// Dummy implementation of a flow-controlled in-memory byte stream.
+
+// For Lab 0, please replace with a real implementation that passes the
+// automated checks run by `make check_lab0`.
+
+// You will need to add private members to the class declaration in `byte_stream.hh`
+
+using namespace std;
+
+ByteStream::ByteStream(const size_t capacity) : _capacity{capacity} {}
+
+size_t ByteStream::write(const string &data) {
+    auto len = min(data.size(), _capacity - _buffer.size());
+    copy(data.cbegin(), data.cbegin() + len, back_inserter(_buffer));
+    _write_count += len;
+    return len;
+}
+
+//! \param[in] len bytes will be copied from the output side of the buffer
+string ByteStream::peek_output(const size_t len) const { return _buffer.substr(0, len); }
+
+//! \param[in] len bytes will be removed from the output side of the buffer
+void ByteStream::pop_output(const size_t len) {
+    _buffer.erase(_buffer.begin(), _buffer.begin() + len);
+    _read_count += len;
+}
+
+//! Read (i.e., copy and then pop) the next "len" bytes of the stream
+//! \param[in] len bytes will be popped and returned
+//! \returns a string
+std::string ByteStream::read(const size_t len) {
+    auto res = peek_output(len);
+    pop_output(len);
+    return res;
+}
+
+void ByteStream::end_input() { _ended = true; }
+
+bool ByteStream::input_ended() const { return _ended; }
+
+size_t ByteStream::buffer_size() const { return _buffer.size(); }
+
+bool ByteStream::buffer_empty() const { return _buffer.empty(); }
+
+bool ByteStream::eof() const { return _ended && buffer_empty(); }
+
+size_t ByteStream::bytes_written() const { return _write_count; }
+
+size_t ByteStream::bytes_read() const { return _read_count; }
+
+size_t ByteStream::remaining_capacity() const { return _capacity - _buffer.size(); }
